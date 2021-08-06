9e899a4694f27da8cd2d828d391b0929b2f9d92c
coderMohammed <86108640+coderMohammed@users.noreply.github.com>
2021-08-06 21:35:18 +0800

    Add files via upload

---
+++ js/CannonBall.js
@@ -0,0 +1,32 @@
+class CannonBall{
+    constructor(x,y){
+        var options = {
+            restitution: 0.5,
+            friction: 1.0,
+            density: 1.0,
+            isStatic: true,
+        }
+        this.r = 40;
+        this.body = Bodies.circle(x,y,this.r,options);
+        World.add(world,this.body);
+        this.image = loadImage("assets/cannonball.png") 
+    }
+    display(){
+        var angle = this.body.angle
+        var pos = this.body.position
+        push();
+        translate(pos.x,pos.y);        
+        rotate(angle)
+        imageMode(CENTER)
+        image(this.image,0,0,this.r,this.r)
+        pop();
+    }
+
+    shoot(){
+        var velocity = p5.Vector.fromAngle(canon.angle);
+        velocity.mult(20);
+        Matter.Body.setStatic(this.body,false)
+        Matter.Body.setVelocity(this.body,{x:velocity.x,y:velocity.y});
+
+    }
+}
